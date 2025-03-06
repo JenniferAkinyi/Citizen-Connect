a22490b1d7301c71c98ea3b8e14c36e283038694
JenniferAkinyi <95017424+Jenniferkinyi@users.noreply.github.com>
2025-03-06 12:57:05 +0300

    fix: correct casing in TaskBoard import path

--- FrontEnd/citizen-connect/src/components/index.js
+++ FrontEnd/citizen-connect/src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Sidebar } from './Sidebar/Sidebar'
-export { default as TaskBoard } from "./dashboard/TaskBoard/Taskboard";
+export { default as TaskBoard } from "./Dashboard/TaskBoard/TaskBoard";
 export { default as AreaCards } from "./AdminPages/AdminDashboard/AreaCards/AreaCards";
 export { default as AreaTable } from "./AdminPages/AdminDashboard/AreaTable/AreaTable";
 export { default as CreatePoll } from "./AdminPages/CreatePoll/CreatePoll";
