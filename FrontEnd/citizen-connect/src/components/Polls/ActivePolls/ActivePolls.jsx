9f09f8e3a309e259ab07ca313ed447a8f3de0a1e
JenniferAkinyi <jennifer.o.akinyi@gmail.com>
2026-05-13 16:22:48 +0300

    Active polls

--- FrontEnd/citizen-connect/src/components/Polls/ActivePolls/ActivePolls.jsx
+++ FrontEnd/citizen-connect/src/components/Polls/ActivePolls/ActivePolls.jsx
@@ -1,16 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
-import './ActivePolls.css'
+import { fetchPolls } from "../../../services/api";
+import "./ActivePolls.css";
 
 const ActivePolls = () => {
-  return (
-    <div className='polls'>
-        <div className='polls-heading'>
-            <h3>Active Polls</h3>
-            <p className='view-polls'>View All <FaArrowRightLong className='icon'/></p>
-        </div>
-    </div>
-  )
-}
+  const [polls, setPolls] = useState([]);
+  const getDaysLeft = (expiresAt) => {
+    const today = new Date();
+    const expiry = new Date(expiresAt);
 
-export default ActivePolls
+    const difference = expiry - today;
+
+    const daysLeft = Math.ceil(difference / (1000 * 60 * 60 * 24));
+
+    if (daysLeft > 1) {
+      return `Ends in ${daysLeft} days`;
+    }
+
+    if (daysLeft === 1) {
+      return "Ends tomorrow";
+    }
+
+    if (daysLeft === 0) {
+      return "Ends today";
+    }
+
+    return "Expired";
+  };
+  useEffect(() => {
+    const getPolls = async () => {
+      try {
+        const response = await fetchPolls();
+
+        const activePolls = response.filter(
+          (poll) => poll.status.toLowerCase() === "active",
+        );
+
+        activePolls.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+        );
+
+        setPolls(activePolls);
+      } catch (error) {
+        console.error("Error fetching polls:", error);
+      }
+    };
+    getPolls();
+  }, []);
+
+  return (
+    <div className="polls">
+      <div className="polls-heading">
+        <h3>Active Polls</h3>
+        <p className="view-polls">
+          View All <FaArrowRightLong className="icon" />
+        </p>
+      </div>
+      <div className="polls-container">
+        {polls.length > 0 ? (
+          polls.map((poll) => (
+            <div className="poll-card" key={poll.id}>
+              <div className="poll-top">
+                <span className="poll-status active">{poll.status}</span>
+
+                <span className="poll-date">{getDaysLeft(poll.expiresAt)}</span>
+              </div>
+              <h4>{poll.question}</h4>
+              <p className="poll-description">{poll.description}</p>
+              <button className="vote-btn">Cast Your Vote</button>
+            </div>
+          ))
+        ) : (
+          <div className="no-polls">
+            <p>No active polls available.</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ActivePolls;
